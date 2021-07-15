d2d1b9fe97a8a8e4135b27dda29a93105b714ddc
Winson Chung <winsonc@google.com>
2021-07-15 10:35:46 -0700

    Add mechanism for a task's windows to be trusted overlays
    - Exposes a method to set that a certain part of the SF hierarchy is
      trusted, and sets this state for tasks in PIP.
    
    Bug: 191529039
    Bug: 196389741
    Test: Manual, try using permission dialog while PIP is active
    
    Change-Id: I170cb5a7d22ef569eb36de21cc0bcbef60dd385e
    Merged-In: I170cb5a7d22ef569eb36de21cc0bcbef60dd385e
    (cherry picked from commit 47fb132ef2c4ca2896e9b82c4f004542139b6ad1)
    Merged-In: I170cb5a7d22ef569eb36de21cc0bcbef60dd385e

--- services/core/java/com/android/server/wm/Task.java
+++ services/core/java/com/android/server/wm/Task.java
@@ -1158,7 +1158,7 @@ class Task extends WindowContainer<WindowContainer> {
                     && (newParent == null || !newParent.inPinnedWindowingMode())) {
                 // Notify if a task from the pinned stack is being removed
                 // (or moved depending on the mode).
-                mAtmService.getTaskChangeNotificationController().notifyActivityUnpinned();
+                mRootWindowContainer.notifyActivityPipModeChanged(this, null);
             }
         }
 
@@ -4655,5 +4655,4 @@ class Task extends WindowContainer<WindowContainer> {
     long getProtoFieldId() {
         return TASK;
     }
-
 }
